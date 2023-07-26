bacff05e76363384afa803eb7ba2139e3432d523
CAUSALITY-3 <70097078+CAUSALITY-3@users.noreply.github.com>
2023-07-26 13:39:44 +0530

    connected to mongodb

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,16 @@ console.log(process.env.HELLO)
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+const mongoose = require('mongoose')
+async function run() {
+  try{
+    await mongoose.connect('mongodb://127.0.0.1:27017/thanal')
+    console.log("connected to db")
+    } catch (e) {
+    console.log("not connected",e)
+  }
+}
+run()
 
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
