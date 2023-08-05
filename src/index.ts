24380f48b1fd91bc1921fc0b2537ac5bd7b037ab
CAUSALITY-3 <70097078+CAUSALITY-3@users.noreply.github.com>
2023-08-05 11:33:35 +0530

    cors and get api

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ console.log(process.env.HELLO);
 const Product = require("./model/products");
 const app = express();
 const port = 5000;
+const cors = require('cors');
 
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
@@ -16,12 +17,14 @@ const User = mongoose.model("User", userSchema);
 mongoose.connect("mongodb://127.0.0.1:27017/thanal");
 console.log("sasi");
 
+app.use(cors());
 app.use(express.json());
 app.post(
   "/products",
   async (req, res) => {
     console.log("sasi", req.body);
-    const product = await Product.create(req.body);
+    const product = await Product.findOneAndUpdate({name: req.body.name}, req.body, {upsert: true})
+    // const product = await Product.create(req.body);
     res.send(product);
     console.log(product);
   },
@@ -29,7 +32,19 @@ app.post(
     res.status(400).send({ error: error.message });
   }
 );
-
+app.get(
+  "/products",
+  async (req, res) => {
+    console.log("sasi", req.body);
+    const product = await Product.find()
+    // const product = await Product.create(req.body);
+    res.send(product);
+    console.log(product);
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
 });
